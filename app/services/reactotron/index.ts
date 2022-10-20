99b801b9b4a217619dbef79d6a88a8a28897bbb7
Robert Li <robert.li@welab.co>
2022-10-20 16:07:51 +0800

    inital commit

---
+++ app/services/reactotron/index.ts
@@ -0,0 +1,3 @@
+/*** This file is loaded for snack to resolve Reactotron for expo snack**/ 
+ export const setupReactotron = (arg1:any) => null 
+export const setReactotronRootStore=(arg1:any, arg2:any)=>null
